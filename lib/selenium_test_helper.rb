bd9d11f32d8faa94f7317a40eac9ae2c71b3110d
Chris Rode <cirode@gmail.com>
2011-11-16 21:26:47 +1100

    Creating a proper gem for the selenium_test_helper

---
+++ lib/selenium_test_helper.rb
@@ -0,0 +1,15 @@
+module SeleniumTestHelper
+  def for_each_browser(browser_names=[:firefox, :chrome], &block)
+    previous_browser = nil
+    at_exit do
+      previous_browser.quit if previous_browser
+    end
+    browser_names.each do |browser_name|
+      driver =  Lazy::Promise.new do 
+        previous_browser.quit if previous_browser
+        previous_browser = Selenium::WebDriver.for browser_name
+      end
+      block.call(driver, browser_name)
+    end
+  end
+end
