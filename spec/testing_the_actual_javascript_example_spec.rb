e2d0f412a39772703435e5455a59acc8063a4c3c
Chris Rode <cirode@gmail.com>
2011-11-18 17:45:21 +1100

    added another test with waiting.. ready for initial release as soon as documentation is done

--- spec/testing_the_actual_javascript_example_spec.rb
+++ spec/testing_the_actual_javascript_example_spec.rb
@@ -9,6 +9,15 @@ describe "test_js" do
       it "should have an h1 with Dynamic Page" do
         browser.find_element(:xpath => '//h1').text.should == "Dynamic Page"
       end
+      context "when I click on the click here anchor" do
+        it "should say clicked here after a delay" do
+          anchor = browser.find_element(:xpath => '//a')
+          anchor.text.should_not == "clicked here"
+          anchor.click
+          wait = Selenium::WebDriver::Wait.new(:timeout => 3)
+          wait.until {anchor.text == "clicked here"}
+        end
+      end
     end
   end
 end
